627280fad30308d74ebff585dfaa49aca20a8b29
James Cherti <60946298+jamescherti@users.noreply.github.com>
2026-01-30 15:19:38 -0500

    Remove calling jit-lock-fontify-now when restoring buffers

--- easysession.el
+++ easysession.el
@@ -1170,29 +1170,6 @@ accordingly, ensuring backward compatibility with legacy session files."
             (let ((target-buffer (or (buffer-base-buffer buffer) buffer)))
               (easysession--ensure-buffer-name target-buffer buffer-name)
 
-              ;; Fixes the issue preventing font-lock-mode from fontifying
-              ;; restored buffers, causing the text to remain unfontified
-              ;; until the user presses a key.
-              ;;
-              ;; TODO: This is inefficient and requires optimization. The Emacs
-              ;; developers appear to have addressed this issue in their
-              ;; upstream codebase.
-              ;;
-              ;; (with-current-buffer target-buffer
-              ;; (when (and (bound-and-true-p font-lock-mode)
-              ;;            (bound-and-true-p
-              ;;             redisplay-skip-fontification-on-input)
-              ;;            (fboundp 'jit-lock-fontify-now))
-              ;;   (run-with-idle-timer
-              ;;    0.1 nil
-              ;;    (lambda ()
-              ;;      (message "Hello %s" (buffer-name))
-              ;;      (condition-case err
-              ;;          (jit-lock-fontify-now)
-              ;;        (error
-              ;;         (easysession--warning
-              ;;          "%s" (error-message-string err))))))))
-
               ;; Restore buffer narrowing if present
               (when new-format-p
                 (easysession--restore-buffer-state target-buffer
