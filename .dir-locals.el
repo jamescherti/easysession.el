f493886cf18a24b842692f229fd79e9ede8f4e04
James Cherti <60946298+jamescherti@users.noreply.github.com>
2026-03-05 19:19:13 -0500

    Update .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,2 +1,3 @@
 ;; pre-commit-elisp .dir-locals.el | https://github.com/jamescherti/pre-commit-elisp
-((nil . ((pre-commit-elisp-error-on-compile-warning . t))))
+((nil . ((pre-commit-elisp-load-path . ("."))
+         (pre-commit-elisp-error-on-compile-warning . t))))
