8a195693ccc144f48d7dedf50e136be9a76b437c
James Cherti <60946298+jamescherti@users.noreply.github.com>
2026-07-04 21:27:52 -0400

    Release: 1.2.5

--- easysession.el
+++ easysession.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2024-2026 James Cherti | https://www.jamescherti.com/contact/
 
 ;; Author: James Cherti <https://www.jamescherti.com/contact/>
-;; Version: 1.2.4
+;; Version: 1.2.5
 ;; URL: https://github.com/jamescherti/easysession.el
 ;; Keywords: convenience
 ;; Package-Requires: ((emacs "26.1"))
