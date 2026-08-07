83a15da810f88d095b9e21619b70b6d801b0aac0
James Cherti <60946298+jamescherti@users.noreply.github.com>
2026-08-07 18:29:39 -0400

    Release: 1.2.6

--- extensions/easysession-magit.el
+++ extensions/easysession-magit.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2026 Emre Yolcu
 
 ;; Author: Emre Yolcu
-;; Version: 1.2.5
+;; Version: 1.2.6
 ;; URL: https://github.com/jamescherti/easysession.el
 ;; Keywords: convenience
 ;; Package-Requires: ((emacs "26.1"))
